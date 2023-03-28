f0a78a0cfa0197f6ec09ac52f9fcc9fd1d321e3f
hangeul <sheet4252@gmail.com>
2023-03-28 18:04:49 +0900

    javadb연습

--- javadb.sql
+++ javadb.sql
@@ -1,1 +1,73 @@
-# javadb
+-- JAVADB
+
+-- USERTBL 테이블 생성
+CREATE TABLE USERTBL(
+NO NUMBER(4) CONSTRAINT PK_USERTBL PRIMARY KEY,
+USERNAME NCHAR(4) NOT NULL,
+BIRTHYEAR NUMBER(4) NOT NULL,
+ADDR NVARCHAR2(30) NOT NULL,
+MOBILE VARCHAR2(15)
+);
+-- NO(숫자 4), USERNAME(이름-한글4이상), BIRTHYEAR(년도-숫자4), ADDR(주소-한글,숫자넉넉하게), MOBILE(010-1234-1234)
+-- NO PK 제약조건 지정(조건명 PK_USERTBL)
+SELECT * FROM USER_CONSTRATINS;
+
+DROP TABLE USERTBL;
+
+-- 시퀀스 생성
+-- user_seq 생성(기본)
+CREATE SEQUENCE USER_SEQ;
+
+SELECT * FROM USERTBL;
+
+
+-- INSERT 
+INSERT INTO USERTBL (NO,USERNAME,BIRTHYEAR, ADDR, MOBILE)
+VALUES(USER_SEQ.NEXTVAL,'홍길동',1997,'대구광역시 북구 1234','010-1234-1234');
+
+COMMIT;
+
+
+
+-- paytype 테이블 : pay_no(숫자-1 pk), info(문자-card, cash)
+-- paytype_seq 생성
+create table paytype(
+pay_no number(1) constraint pk_paytype primary key,
+info char(4) not null);
+create sequence paytype_seq;
+
+insert into paytype values(paytype_seq.nextval,'card');
+insert into paytype values(paytype_seq.nextval,'cash');
+
+select * from paytype; -- 1 : card,   2 : cash
+
+-- suser : user_id(숫자-4 pk), name(문자-한글), pay_no(숫자-1 : paytype테이블에 있는 pay_no참조 해서 사용)
+create table suser(
+user_id number(4) constraint pk_user_id primary key,
+name nchar(4) not null,
+pay_no number(1) constraint user_pay_no_fk references paytype(pay_no));
+
+
+-- product
+-- product_id(숫자-8자리-pk),pname(문자넉넉하게), price(숫자 가격), content(문자 정보 넉넉하게)
+create table product(
+product_id number(8) primary key,
+pname varchar2(20) not null,
+price number(10) not null,
+content varchar2(50));
+
+create sequence product_seq;
+
+drop table product;
+
+-- sorder
+-- order_id(숫자-8 pk), user_id(suser 테이블의 user_id참조), product_id(product 테이블의 product_id 참조)
+-- order_seq 생성
+create table sorder(
+order_id number(8) primary key,
+user_id number(4) not null references suser(user_id),
+product_id number(8) not null references product(product_id));
+
+create sequence order_seq;
+
+commit;
--- system.sql
+++ system.sql
@@ -1,3 +1,14 @@
+-- 사용자 : scott, hr => 데이터베이스 접속하여 데이터를 관리하는 계정
+--        테이블, 인덱스, 뷰 등 여러 객체가 사용자별로 생성됨
+--        업무에 따라 사용자들을 나눠서 관리
+
+-- 스키마 : 데이터칸 관계, 데이터 구조, 제약조건 등 데이터를 관리 저장하기 위해 정의한 데이터베이스 구조의 범위
+-- 스키마 == 사용자 (오라클 데이터베이스 경우)
+
+
+
+
+-- 사용자 생성 : 사용자 생성 권한이 필요함 ==> sys, system
 --# 대소문자 구별 하지 않음(단 비밀번호는 구별을 함)
 --# CREATE : 생성 / ALTER : 수정 / DROP : 삭제, DELETE : 삭제 
 --
@@ -14,6 +25,8 @@ TEMPORARY TABLESPACE TEMP;
 
 --# GRANT : 권한 부여(사용자만 생성해서는 아무것도 할 수 없음) 
 GRANT CONNECT, RESOURCE TO JAVADB;
+
+ALTER USER JAVADB QUOTA 2M ON USERS;
 
 
 CREATE USER SCOTT IDENTIFIED BY TIGER
@@ -35,3 +48,60 @@ GRANT UNLIMITED TABLESPACE TO SCOTT;
 GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
 ALTER USER SCOTT DEFAULT TABLESPACE USERS;
 ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
+
+
+-- test 사용자 생성 // 비번 12345
+CREATE USER test IDENTIFIED BY 12345;
+
+-- 접속권한 부여하지 않으면 안됨.
+
+-- 권한관리
+-- 1) 시스템 권한
+--    사용자생성(CREATE USER) / 수정(ALTER USER) / 삭제(DROP USER)
+--    데이터베이스 접근(CREATE SESSION) // 수정(ALTER SESSION)
+--    여러 객체 생성(VIEW, SYNONYM 및 관리 권한)
+
+-- 2) 객체 권한
+--    테이블 수정, 삭제, 인덱스 생성, 삽입, 참조, 조회, 수정
+--    뷰 삭제, 삽입, 생성, 조회, 수정
+--    시퀀스 수정, 조회
+--    프로시저, 
+
+
+
+
+
+-- 권한 부여
+
+-- CREATE SESSION 을 test에게 부여
+GRANT CREATE SESSION TO test;
+
+GRANT RESOURCE, CREATE TABLE TO TEST;
+
+-- 테이블 스페이스 USERS에 권한이 없습니다.
+ALTER USER TEST DEFAULT TABLESPACE USERS;
+ALTER USER TEST TEMPORARY TABLESPACE TEMP;
+ALTER USER TEST QUOTA 2M ON USERS;  -- USERS에 공간 설정
+
+-- SCOTT 에게 TEST가 생성한 MEMBER 테이블 조회 권한 부여
+-- GRANT SELECT ON MEMBER TO SCOTT;
+
+-- 권한 취소
+-- REVOKE SELECT, INSERT ON MEMBER FROM SCOTT;
+
+-- 롤(ROLL) : 여러 권한들의 모임
+-- CONNECT 롤 : CREATE SESSION
+-- RESOURCE 롤 : CREATE TRIGGER, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TABLE...
+
+-- 사용자 삭제
+DROP USER TEST;
+DROP USER TEST CASCADE;  -- TEST와 관련된 모든 객체 같이 삭제
+
+
+-- 사용자 생성 + 테이블 스페이스 권한
+CREATE USER TEST2 IDENTIFIED BY 12345
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
+
+-- 권한 부여
+GRANT CONNECT, RESOURCE TO TEST2;
